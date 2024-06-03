fb987bec3e7e222ee63af3af2d3d333b23d55005
Deepcoders30 <umair.khan.rocko@gmail.com>
2024-06-04 03:45:21 +0530

    feat: Lesson Buttons

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { challengeProgress, courses, units, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -21,6 +21,52 @@ export const getUserProgress = cache(async () => {
   return data;
 });
 
+export const getUnits = cache(async () => {
+  const { userId } = await auth();
+  const userProgress = await getUserProgress();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges: boolean = lesson.challenges.every(
+        (challenge) => {
+          return (
+            challenge.challengeProgress &&
+            challenge.challengeProgress.length > 0 &&
+            challenge.challengeProgress.every((progress) => {
+              progress.completed;
+            })
+          );
+        }
+      );
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonWithCompletedStatus };
+  });
+
+  return normalizedData;
+});
+
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
 
@@ -33,4 +79,9 @@ export const getCourseById = cache(async (courseId: number) => {
   });
 
   return data;
+});
+
+export const getCourseProgress = cache(async () => {
+  const userId=await auth();
+  const userProgress=await getUserProgress();
 });
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -51,6 +51,76 @@ const main = async () => {
       },
     ]);
 
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Nouns",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Verbs",
+      },
+      {
+        id: 3,
+        unitId: 1,
+        order: 3,
+        title: "Nouns",
+      },
+      {
+        id: 4,
+        unitId: 1,
+        order: 4,
+        title: "Nouns",
+      },
+      {
+        id: 5,
+        unitId: 1,
+        order: 5,
+        title: "Nouns",
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
     console.log("Seeding Finished");
   } catch (error) {
     console.error(error);
