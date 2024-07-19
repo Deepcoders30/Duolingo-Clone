3c3fbf1bc2b256670a03e5a7d6281e69ea6dc302
Deepcoders30 <umair.khan.rocko@gmail.com>
2024-07-19 06:05:13 +0530

    feat: stripe checkout

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
 import { relations } from "drizzle-orm";
+import { timestamp } from "drizzle-orm/pg-core";
 import {
   boolean,
   integer,
@@ -133,3 +134,13 @@ export const challengeProgressRelations = relations(
     }),
   })
 );
+
+export const userSubscription = pgTable("user_subscription", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull().unique(),
+  stripeCustomerId: text("stripe_customer_id").notNull().unique(),
+  stripeSubscriptionId: text("stripe_subscription_id").notNull().unique(),
+  stripePriceId: text("stripe_price_id").notNull(),
+  stripeCurrentPeriodEnd: timestamp("stripe_current_period_end").notNull(),
+  
+});
